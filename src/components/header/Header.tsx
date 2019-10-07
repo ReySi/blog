811cfeb8465a1fc6870d42d9a416401704a7a2f4
Simon <srey@inovex.de>
2019-10-07 15:38:10 +0200

    reformat spacing and attempt to create sticky tab bar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,22 +3,31 @@ import './Header.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import TypoGraphy from '@material-ui/core/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
-import IconButton from '@material-ui/core/IconButton';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 const Header: React.FC = () => (
-    <div className="header">
-        <AppBar title="Simons Blog" position="static">
-            <Toolbar>
-                <IconButton>
-                    <MenuIcon/>
-                </IconButton>
-                <TypoGraphy variant="h6">
-                    My header
-                </TypoGraphy>
-            </Toolbar>
-        </AppBar>
-    </div>
+  <div className="header">
+    <AppBar title="Simons Blog" position="static">
+      <Toolbar>
+        <TypoGraphy variant="h6">
+            My header
+        </TypoGraphy>
+      </Toolbar>
+      <div className="sticky-tab-bar">
+        <Tabs
+          value="irgendwas"
+          indicatorColor="primary"
+          textColor="primary"
+          centered
+        >
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </div>
+    </AppBar>
+  </div>
 );
 
 export default Header;
